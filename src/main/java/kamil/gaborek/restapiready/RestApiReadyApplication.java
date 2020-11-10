5a6844b634a28e1351eca1718b2eee6c2680296e
Kamil Gaborek <BFK678@motorolasolutions.com>
2020-11-10 14:01:46 +0100

    init commit

---
+++ src/main/java/kamil/gaborek/restapiready/RestApiReadyApplication.java
@@ -0,0 +1,13 @@
+package kamil.gaborek.restapiready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiReadyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiReadyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kamil/gaborek/restapiready/RestApiReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package kamil.gaborek.restapiready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiReadyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
